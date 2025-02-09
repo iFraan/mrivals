3f6becda7c6adc6285248dc6370c25f591280ed7
Francisco Zorat <franciscozorat@gmail.com>
2025-02-09 02:42:06 -0300

    refactor: rename project from vandal.js to mrivals and update related metadata

--- test.ts
+++ test.ts
@@ -6,8 +6,6 @@ const test = async () => {
         console.log('User:', user.info());
         console.log('Ranked:', user.ranked());
         console.log('Unrated: ', user.unrated());
-        console.log('Agents: ', user.agents());
-        console.log('ALL GAMEMODES (including deathmatch, spike-rush, etc) ', user.gamemodes());
         console.log('Raw: ', user.raw());
     } catch (e) {
         console.log(e);
